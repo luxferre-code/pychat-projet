99ab268bb2e7483bfedae49d7e710e3201765f5e
TheAngelLCF <65764028+TheAngelLCF@users.noreply.github.com>
2022-02-22 13:44:17 +0000

    Première mise à jours
    Côtés Valentin:
         Création de la fonction 'create_unique_id'
         Création de la fonction 'make_client'
    Côtés Juliann:
        Création de la fonction 'get_client'

--- server/background.py
+++ server/background.py
@@ -1,1 +1,69 @@
 # Background file on Client
+
+import sqlite3
+from random import randint
+
+database = "db_server.db"
+
+# Création d'un client
+
+def create_unique_id() -> int:
+    """
+    Fonction permettant de créer un id unique pour un utilisateur
+    
+    param:
+    None
+    
+    Return type: integer
+    # Valentin Thuillier
+    """
+    connect = sqlite3.connect(database)
+    while True:
+        temps = str(randint(0, 9))
+        for _ in range(9):
+            temps += str(randint(0, 9))
+        cursor.execute('SELECT count(id) FROM Clients WHERE id = ?', (temps, ))
+        data = cursor.fetchone()[0]
+        if(data == 0): break
+    return temps
+
+def make_client(pseudo: str, password: str, mail: str) -> bool:
+    """
+    Fonction permettant la création d'un utilisateur sur la base de donnée serveur
+    
+    param:
+    pseudo: string (3 <= taille <= 12)
+    password: string (crypté en sha256)
+    mail: string
+    
+    Return type: boolean (True si réussit, False si pas réussi)
+    # Valentin Thuillier
+    """
+    assert isinstance(pseudo, string) and len(pseudo) >= 3 and len(pseudo) <= 12, "Impossible de créer un compte client avec un pseudo ne respectant pas les régles !"
+    assert isinstance(password, string), "Merci d'entrer un mot de passe crypté en sha256 !"
+    assert isinstance(mail, string) and "@" in mail, "Merci d'entrer un mail valide !"
+    
+    try:
+        connect = sqlite3.connect(database)
+        cursor = connect.cursor()
+        cursor.execute('INSERT INTO Clients VALUES (?, ?, ?, ?)', (create_unique_id, pseudo, password, mail))
+        connect.commit()
+        connect.close()
+        return True
+    except:
+        return False
+
+def get_client(idd: int):
+    """Récupère les informations du client ( nom et mail)
+    Parameters:
+        id : int
+    # Juliann Lestrelin
+    """
+    
+    assert type(idd) == int, "L'id est invalide"
+    connexion = sqlite3.connect(database)
+    cursor = connexion.cursor()
+    cursor.execute("SELECT name, mail FROM Clients WHERE id == ?", (idd,))
+    
+    return cursor.fechone()[0]
+    
