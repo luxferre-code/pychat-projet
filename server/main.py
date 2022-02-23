bd4325debcaa04da10c2ead17f9c8fac625709c6
TheAngelLCF <65764028+TheAngelLCF@users.noreply.github.com>
2022-02-23 21:16:39 +0100

    Mise en place de la mise à jour auto des modules

--- server/main.py
+++ server/main.py
@@ -1,1 +1,19 @@
 # Main file on Server
+import pip
+
+
+def installer(to_install: str) -> None:
+  """
+  Fonction permettant d'installer des modules
+  
+  param:
+  to_install: string
+  
+  Return type: None
+  # Valentin Thuillier
+  """
+  pip.main(["install", to_install])
+
+
+if __name__ == "__main__":
+  to_install = ["sqlite3"]
