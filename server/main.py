25899cbdee2bced2c995d5fac436d011c2ed97f4
TheAngelLCF <65764028+TheAngelLCF@users.noreply.github.com>
2022-02-23 21:20:03 +0100

    Update main.py

--- server/main.py
+++ server/main.py
@@ -3,17 +3,20 @@ import pip
 
 
 def installer(to_install: str) -> None:
-  """
-  Fonction permettant d'installer des modules
+    """
+    Fonction permettant d'installer des modules
   
-  param:
-  to_install: string
+    param:
+    to_install: string
   
-  Return type: None
-  # Valentin Thuillier
-  """
-  pip.main(["install", to_install])
+    Return type: None
+    # Valentin Thuillier
+    """
+    pip.main(["install", to_install])
 
 
 if __name__ == "__main__":
-  to_install = ["sqlite3"]
+    pip.main(["install", "--upgrade", "pip"])
+    to_install = ["sqlite3"]
+    for elt in to_install:
+        installer(elt)
